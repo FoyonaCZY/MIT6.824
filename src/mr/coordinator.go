e19dc789d7c1212bfdf98cb57f2ce9fa9ce58a72
FoyonaCZY <czydyx1@126.com>
2024-12-19 22:52:22 +0800

    Lab1

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,69 +1,196 @@
 package mr
 
-import "log"
+import (
+	"log"
+	"sync"
+	"time"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
+type taskState int
 
-type Coordinator struct {
-	// Your definitions here.
+const (
+	Idle taskState = iota
+	InProgress
+	Completed
+)
 
+type Task struct {
+	Type      string // "map" , "reduce" , "wait" or "exit"
+	State     taskState
+	ID        int
+	NMap int
+	NReduce int
+	InputFile string
+	StartTime int64
 }
 
-// Your code here -- RPC handlers for the worker to call.
+type Coordinator struct {
+	mu            sync.Mutex
+	mapTasks      []Task
+	reduceTasks   []Task
+	nMap          int
+	nReduce       int
+	allMapDone    bool
+	allReduceDone bool
+}
 
-//
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+func (c *Coordinator) RequestTask(args *RequestArgs, reply *TaskReply) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if !c.allMapDone {
+		//检查是否有map任务
+		for i := 0; i < c.nMap; i++ {
+			//检测超时任务
+			if c.mapTasks[i].State == InProgress && time.Now().Unix()-c.mapTasks[i].StartTime > 10 {
+				c.mapTasks[i].State = Idle
+			}
+
+			//返回一个空闲的map任务
+			if c.mapTasks[i].State == Idle {
+				c.mapTasks[i].State = InProgress
+				c.mapTasks[i].StartTime = time.Now().Unix()
+				reply.Task = c.mapTasks[i]
+				log.Printf("Sending task: %+v, nMap: %d, nReduce: %d", reply.Task, reply.Task.NMap, reply.Task.NReduce)
+				return nil
+			}
+		}
+
+		//暂时没有空闲任务，通知worker等待
+		reply.Task = Task{
+			Type: "wait",
+		}
+		return nil
+	}
+	if !c.allReduceDone {
+		//检查是否有reduce任务
+		for i := 0; i < c.nReduce; i++ {
+			//检测超时任务
+			if c.reduceTasks[i].State == InProgress && time.Now().Unix()-c.reduceTasks[i].StartTime > 10 {
+				c.reduceTasks[i].State = Idle
+			}
+
+			//返回一个空闲的reduce任务
+			if c.reduceTasks[i].State == Idle {
+				c.reduceTasks[i].State = InProgress
+				c.reduceTasks[i].StartTime = time.Now().Unix()
+				reply.Task = c.reduceTasks[i]
+				return nil
+			}
+		}
+
+		//暂时没有空闲任务，通知worker等待
+		reply.Task = Task{
+			Type: "wait",
+		}
+		return nil
+	}
+	//所有任务都完成,通知worker退出
+	reply.Task = Task{
+		Type: "exit",
+	}
 	return nil
 }
 
+// TaskDone 告诉coordinator任务已经完成
+func (c *Coordinator) TaskDone(args *RequestArgs, reply *TaskReply) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
-//
+	if args.TaskType == "map" {
+		c.mapTasks[args.TaskID].State = Completed
+	} else if args.TaskType == "reduce" {
+		c.reduceTasks[args.TaskID].State = Completed
+	}
+
+	//检查是否所有map任务都完成
+	c.allMapDone = true
+	for i := 0; i < c.nMap; i++ {
+		if c.mapTasks[i].State != Completed {
+			c.allMapDone = false
+			break
+		}
+	}
+
+	//检查是否所有reduce任务都完成
+	c.allReduceDone = true
+	for i := 0; i < c.nReduce; i++ {
+		if c.reduceTasks[i].State != Completed {
+			c.allReduceDone = false
+			break
+		}
+	}
+
+	if c.allMapDone && c.allReduceDone {
+		log.Println("all tasks done")
+		time.Sleep(3 * time.Second)
+	}
+
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
-	rpc.Register(c)
+	err := rpc.Register(c)
+	if err != nil {
+		return
+	}
 	rpc.HandleHTTP()
-	//l, e := net.Listen("tcp", ":1234")
 	sockname := coordinatorSock()
-	os.Remove(sockname)
+	_ = os.Remove(sockname)
 	l, e := net.Listen("unix", sockname)
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
-	go http.Serve(l, nil)
+	go func() {
+		err := http.Serve(l, nil)
+		if err != nil {
+
+		}
+	}()
 }
 
-//
-// main/mrcoordinator.go calls Done() periodically to find out
-// if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	//当所有map和reduce任务都完成时，返回true
+	return c.allMapDone && c.allReduceDone
 }
 
-//
-// create a Coordinator.
-// main/mrcoordinator.go calls this function.
-// nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		mapTasks:    make([]Task, len(files)),
+		reduceTasks: make([]Task, nReduce),
+		nMap:        len(files),
+		nReduce:     nReduce,
+	}
 
-	// Your code here.
+	//初始化map任务
+	for i, file := range files {
+		c.mapTasks[i] = Task{
+			Type:      "map",
+			State:     Idle,
+			ID:        i,
+			InputFile: file,
+			NMap:c.nMap,
+			NReduce:c.nReduce,
+		}
+	}
 
+	//初始化reduce任务
+	for i := 0; i < nReduce; i++ {
+		c.reduceTasks[i] = Task{
+			Type:    "reduce",
+			State:   Idle,
+			ID:      i,
+			NMap:c.nMap,
+			NReduce:c.nReduce,
+		}
+	}
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,85 +1,78 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
-// Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
-// use ihash(key) % NReduce to choose the reduce
-// task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
-	h.Write([]byte(key))
+	_, err := h.Write([]byte(key))
+	if err != nil {
+		return 0
+	}
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
-// main/mrworker.go calls this function.
-//
+// Worker main/mrworker.go calls this function.
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
+	for {
+		reply := TaskReply{}
+		ok := call("Coordinator.RequestTask", &RequestArgs{}, &reply)
 
-	// Your worker implementation here.
+		log.Printf("Received task: %+v, nMap: %d, nReduce: %d", reply.Task, reply.Task.NMap, reply.Task.NReduce)
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+		if !ok {
+			break
+		}
+		task := reply.Task
 
-}
+		//检测任务类型
+		if task.Type == "map" {
+			//执行map任务
+			doMap(task, reply.Task.NReduce, mapf)
+		}
+		if task.Type == "reduce" {
+			//执行reduce任务
+			doReduce(task, reply.Task.NMap, reducef)
+		}
+		if task.Type == "wait" {
+			time.Sleep(1 * time.Second)
+		}
+		if task.Type == "exit" {
+			break
+		}
 
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
+		//通知Master任务完成
+		call("Coordinator.TaskDone", &RequestArgs{TaskID: task.ID, TaskType: task.Type}, &TaskReply{})
 	}
 }
 
-//
-// send an RPC request to the coordinator, wait for the response.
-// usually returns true.
-// returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
 	c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
-	defer c.Close()
+	defer func(c *rpc.Client) {
+		err := c.Close()
+		if err != nil {
+
+		}
+	}(c)
 
 	err = c.Call(rpcname, args, reply)
 	if err == nil {
@@ -88,4 +81,118 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	fmt.Println(err)
 	return false
+}
+
+// doMap 执行map任务
+func doMap(task Task, nReduce int, mapf func(string, string) []KeyValue) {
+	//读取文件内容
+	filename := task.InputFile
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	err = file.Close()
+	if err != nil {
+		return
+	}
+
+	kva := mapf(filename, string(content))
+	//将map结果写入中间文件
+	intermediate := make([][]KeyValue, nReduce)
+	for _, kv := range kva {
+		reduceTask := ihash(kv.Key) % nReduce
+		intermediate[reduceTask] = append(intermediate[reduceTask], kv)
+	}
+
+	//将中间文件写入磁盘
+	for i, kva := range intermediate {
+		oname := fmt.Sprintf("mrx-%d-%d", task.ID, i)
+		ofile, _ := os.Create(oname)
+		enc := json.NewEncoder(ofile)
+		for _, kv := range kva {
+			err := enc.Encode(&kv)
+			if err != nil {
+				log.Fatalf("cannot encode %v", kv)
+			}
+		}
+		err := os.Rename(oname, fmt.Sprintf("mr-%d-%d", task.ID, i))
+		if err != nil {
+			return
+		}
+
+		err = ofile.Close()
+		if err != nil {
+			return
+		}
+	}
+}
+
+// doReduce 执行reduce任务
+func doReduce(task Task, nMap int, reducef func(string, []string) string) {
+	//根据任务ID读取中间文件
+	var intermediate []KeyValue
+	for i := 0; i < nMap; i++ {
+		filename := fmt.Sprintf("mr-%d-%d", i, task.ID)
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", filename)
+		}
+		// 读取文件内容
+		decoder := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := decoder.Decode(&kv); err != nil {
+				break
+			}
+			intermediate = append(intermediate, kv)
+		}
+		err = file.Close()
+		if err != nil {
+			return
+		}
+	}
+
+	// 按键排序（方便分组）
+	sort.Slice(intermediate, func(i, j int) bool {
+		return intermediate[i].Key < intermediate[j].Key
+	})
+
+	// 对每个键调用 Reduce 函数
+	oname := fmt.Sprintf("mr-xout-%d", task.ID) //临时文件
+	ofile, _ := os.Create(oname)
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		var values []string
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// 写入最终输出文件
+		_, err := fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+		if err != nil {
+			return
+		}
+
+		i = j
+	}
+	//重命名文件，保证原子性
+	err := os.Rename(oname, fmt.Sprintf("mr-out-%d", task.ID))
+	if err != nil {
+		return
+	}
+
+	//关闭文件
+	err = ofile.Close()
+	if err != nil {
+		return
+	}
 }
